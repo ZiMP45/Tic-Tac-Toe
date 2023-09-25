1a3bd93487aa9050ecfb66288837fe6c882d4c24
Zak Porche <93185579+ZiMP45@users.noreply.github.com>
2023-09-25 01:17:59 +0000

    working modal for game start

---
+++ app.js
@@ -0,0 +1,15 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
